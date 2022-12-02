2f6f8b0bd4113fd3668ad8d9a88880c7ad69a1d4
Burton Smith <burton@breakstuff.io>
2022-12-02 08:48:41 -0500

    add initial logic

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {
+    generateCustomDataFile,
+    logPluginInit,
+    setComponentReferences,
+    updateConfig,
+  } from "./web-type-generator/generator.js";
+  import type { Options, Params } from "../types";
+  
+  export function generateCustomData(params: Options = {}) {
+    updateConfig(params);
+  
+    return {
+      name: "cem-plugin-vs-code-custom-data-generator",
+      // @ts-ignore
+      analyzePhase({ ts, node, moduleDoc }) {
+        setComponentReferences(ts, node, moduleDoc);
+      },
+      packageLinkPhase({ customElementsManifest }: Params) {
+        logPluginInit();
+        generateCustomDataFile(customElementsManifest);
+      },
+    };
+  }
+  
